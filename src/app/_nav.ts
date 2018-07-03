37afd973bc7a665562ad0f80f4d665435902e7d2
Mohammad Raheem <Abdurraheem@users.noreply.github.com>
2018-07-03 19:03:24 +0530

    Update _nav.ts

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -204,18 +204,5 @@ export const navigation = [
         icon: 'icon-star'
       }
     ]
-  },
-  {
-    name: 'Download CoreUI',
-    url: 'http://coreui.io/angular/',
-    icon: 'icon-cloud-download',
-    class: 'mt-auto',
-    variant: 'success'
-  },
-  {
-    name: 'Try CoreUI PRO',
-    url: 'http://coreui.io/pro/angular/',
-    icon: 'icon-layers',
-    variant: 'danger'
   }
 ];
